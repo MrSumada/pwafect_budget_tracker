a2085ab886623ad6e2d7f507d91bfb3427c17b81
MrSumada <ricksamericana.com>
2022-04-21 15:05:11 -0400

    add webpack, webpack-pwa-manifest

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+    entry: {
+        index: './public/js/index.js',
+        idb: './public/js/idb.js'
+    },
+    output: {
+        path: path.join(__dirname + "/dist"),
+        filename: "[name].bundle.js",
+        publicPath: ''
+    },
+    module: {
+        rules: [
+            {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                loader: 'file-loader',
+                options: {
+                    esModule: false,
+                    name(file) {
+                    return '[path][name].[ext]';
+                    },
+                    publicPath(url) {
+                    return url.replace('../', '/assets/');
+                    }
+                }
+                },
+                {
+                loader: 'image-webpack-loader'
+                }
+            ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static'
+        }),
+        new WebpackPwaManifest({
+            name: "Pwafect Budget Tracker",
+            short_name: "Pwafect",
+            description: "An app for tracking your budget.",
+            start_url: "../index.html",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve("public/icons/icon-512x512.png"),
+                sizes: [72, 96, 128, 144, 152, 192, 384, 512],
+                destination: path.join("assets", "icons")
+            }]
+        })
+    ],
+    mode: 'development'
+};
+
+module.exports = config;
+    
+
+
+
